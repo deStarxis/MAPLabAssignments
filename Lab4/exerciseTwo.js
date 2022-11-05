a9a213e5d1050385fbece84dec5c2cdaec12f305
deStarxis <humagainuzjwol@gmail.com>
2022-11-05 14:07:52 -0500

    added exercise Two

---
+++ Lab4/exerciseTwo.js
@@ -0,0 +1,25 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+  logger() {
+    console.log(`Hello this is ${this.name}`);
+  }
+}
+
+class DecoratedUser {
+  constructor(user, street, city) {
+    this.user = user;
+    this.name = user.name;
+    this.street = street;
+    this.city = city;
+  }
+  logger() {
+    console.log(`Hello this is ${this.name} from ${this.street},${this.city}`);
+  }
+}
+
+const user = new User("Kelly");
+user.logger();
+const decorated = new DecoratedUser(user, "Broadway", "New York");
+decorated.logger();
