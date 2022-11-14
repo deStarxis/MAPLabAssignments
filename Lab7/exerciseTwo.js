3a3e469a848d2b3d5e1ec525e109fb1875d4e3e6
deStarxis <humagainuzjwol@gmail.com>
2022-11-14 10:47:17 -0600

    added solution

--- Lab7/exerciseTwo.js
+++ Lab7/exerciseTwo.js
@@ -1,4 +1,4 @@
-Array.prototype.removeDuplicatesAsync = function () {
+Array.prototype.removeDuplicates = function () {
   let arr = this;
   new Promise(function (resolve, reject) {
     resolve([...new Set(arr)]);
@@ -6,7 +6,7 @@ Array.prototype.removeDuplicatesAsync = function () {
 };
 
 console.log(`start`);
-[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync();
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicates();
 console.log(`end`);
 
 // Output:
